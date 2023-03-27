55a2cf7d17bd92013cd5856397b3211725778521
lai2-01 <lai2-01@itismeucci.com>
2023-03-27 10:37:20 +0000

    tabella ma il cazzo di bottone non funge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import {MatTableModule} from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonComponent } from './button/button.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    ButtonComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/button/button.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic buttons
+ */
+@Component({
+  selector: 'my-button',
+  templateUrl: 'button.component.html',
+  styleUrls: ['button.component.css'],
+})
+export class ButtonComponent {}
